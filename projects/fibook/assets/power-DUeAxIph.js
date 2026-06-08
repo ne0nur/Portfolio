8fb81ea8f5c5ece1984d2dc58632251ea21d6336
Onur Oezel <o.oezel@outlook.com>
2026-06-08 16:53:50 +0200

    feat(projects): add CityCab (featured), fibook (bereinigt) + restructure project grid

---
+++ projects/fibook/assets/power-DUeAxIph.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-IDqHKa5S.js";/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["rect",{x:"14",y:"14",width:"4",height:"6",rx:"2",key:"p02svl"}],["rect",{x:"6",y:"4",width:"4",height:"6",rx:"2",key:"xm4xkj"}],["path",{d:"M6 20h4",key:"1i6q5t"}],["path",{d:"M14 10h4",key:"ru81e7"}],["path",{d:"M6 14h2v6",key:"16z9wg"}],["path",{d:"M14 4h2v6",key:"1idq9u"}]],a=e("binary",t);/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M12 2v10",key:"mnfbl"}],["path",{d:"M18.4 6.6a9 9 0 1 1-12.77.04",key:"obofu9"}]],r=e("power",h);export{a as B,r as P};
